d8f2c6e51d15bc59577694190bf4973147cba772
Gabriela Figueroa <gabfigueroa@icloud.com>
2024-01-27 11:59:32 -0300

    adding functions for simple api request

--- script/main.js
+++ script/main.js
@@ -20,3 +20,51 @@ const showScreen = (screenName) => {
 // cleaner //
 
 const cleanContainer = (selector) => $(selector).innerHTML = ""
+
+
+// API
+
+const urlBase = `http://gateway.marvel.com/v1/public/`
+let ts = `ts=1`
+const publicKey = `&apikey=fce7b36328723d964caf6df4a1aa294c`
+const hash = `&hash=7391644ad1562b5dbed2616db50485d4`
+
+const defineResourceRequest = (resource , resourceID , plus) => {
+    let baseURL = `${urlBase}/${resource}`
+
+    if (!resourceID && !plus) {
+        baseURL
+    }
+
+    if (resourceID) {
+        baseURL += `/${resourceID}`
+    }
+    if (plus) {
+        baseURL += `/${plus}`
+    }
+    return baseURL
+}
+
+const definePath = (resource , resourceID , plus) => {
+    const request = defineResourceRequest(resource , resourceID , plus)
+    const url = `${request}?${ts}${publicKey}${hash}`
+    return url
+}
+
+const requestData = async() => {
+    const path = definePath("comics")
+    const response = await fetch(path)
+    const data = await response.json()
+    console.log(data)
+    return data
+}
+
+requestData()
+
+// const printResults = () => {
+
+// }
+
+// const initialize = () => {
+//     requestData(comics)
+// }
